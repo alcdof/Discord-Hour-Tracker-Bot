ef601cd9e930c46f9775ea1fceabd8f75b986f74
André Caillot <andreluizfilho@alunos.utfpr.edu.br>
2026-03-15 15:03:13 -0300

    turned the bot into a slash command application.

--- main.py
+++ main.py
@@ -1,87 +1,134 @@
 import discord
 import datetime
 from discord.ext import commands
+from discord import app_commands
 from dotenv import load_dotenv
 import os
-
-intents = discord.Intents.all()
-bot = commands.Bot(".", intents=intents)
-cont = 0
-
+from typing import Optional
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-times = {}
+intents = discord.Intents.all()
+
+bot = discord.Client(intents=intents)
+tree = app_commands.CommandTree(bot)
+GUILD_ID = int(os.getenv("GUILD_ID"))
 
 @bot.event
 async def on_ready():
+    await tree.sync(guild=discord.Object(id=GUILD_ID))
     print("Bot successfully initialized.")
 
+cont = 0
+times = {}
+
+# hello
+@tree.command(
+        name="hello",
+        description="the bot greets you.",
+        guild=discord.Object(id=GUILD_ID)
+)
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Hello, {interaction.user.mention}!  👋")
+
 # ping
-@bot.command()
-async def ping(ctx):
+@tree.command(
+        name="ping",
+        description="the bot tells you what is the latency of the server.",
+        guild=discord.Object(id=GUILD_ID)
+)
+async def ping(interaction: discord.Interaction):
     ping = round(bot.latency * 1000)
-    await ctx.reply(f"Pong!\t🏓\t{ping} ms")
+    await interaction.response.send_message(f"Pong!\t🏓\t{ping} ms")
 
 #contador simples: aumenta e diminui
-@bot.command()
-async def contador(ctx, action=None, value: int=None):
+@tree.command(
+        name="counter",
+        description="a simple counter. for all users.",
+        guild=discord.Object(id=GUILD_ID)
+)
+@app_commands.describe(
+    action="increment (inc), decrement (dec) or reset (reset) the counter",
+    value="how many times to increment/decrement"
+)
+async def counter(
+    interaction: discord.Interaction, 
+    action: Optional[str]=None, 
+    value: Optional[int]=None
+):
     global cont
-
+    
     if action == "inc":
         if value is None:
             cont += 1
         else:
             cont += value
-
-    elif action == "sub":
+    elif action == "dec":
         if value is None:
             cont -= 1
         else:
             cont -= value
-
     elif action == "reset":
         cont = 0
-
-    await ctx.reply(f"Counter: {cont}")
+    
+    await interaction.response.send_message(f"Counter: {cont}")
 
 # limpa o chat
-@bot.command()
-async def clear(ctx, value: int=10, user: discord.Member=None):
+@tree.command(
+        name="clear",
+        description="clears the chat.",
+        guild=discord.Object(id=GUILD_ID)
+)
+@app_commands.describe(
+    value="how many messages to delete",
+    user="which user to delete messages"
+)
+async def clear(
+    interaction: discord.Interaction, 
+    value: Optional[int] = 10, 
+    user: Optional[discord.Member] = None
+):
     def check(msg):
         return msg.author == user
     
     if user is None:
-        deleted = await ctx.channel.purge(limit=value)
+        deleted = await interaction.channel.purge(limit=value)
     else: 
-        deleted = await ctx.channel.purge(limit=value, check=check)
+        deleted = await interaction.channel.purge(limit=value, check=check)
 
-    await ctx.send(f"{len(deleted)} deleted messages.")
-
+    await interaction.response.send_message(f"{len(deleted)} deleted messages.")
 
 # entra no canal em que o usuário está
-@bot.command()
-async def connect(ctx):
-    if ctx.author.voice is None:
-        await ctx.reply(f"You need to be on a voice chat.")
+@tree.command(
+        name="join",
+        description="the bot joins the voice channel.",
+        guild=discord.Object(id=GUILD_ID)
+)
+async def join(interaction: discord.Interaction):
+    if interaction.user.voice is None:
+        await interaction.response.send_message("You need to be on a voice chat.")
         return
     
-    channel = ctx.author.voice.channel
+    channel = interaction.user.voice.channel
     await channel.connect()
-    await ctx.reply(f"I successfully joined the **{channel}** channel!")
+    await interaction.response.send_message(f"I successfully joined the **{channel}** channel!")
 
 # sai do canal em que o usuário está
-@bot.command()
-async def leave(ctx):
-    if ctx.voice_client is None:
-        await ctx.reply(f"I'm not on a voice channel.")
+@tree.command(
+        name="leave",
+        description="the bot leaves the voice channel.",
+        guild=discord.Object(id=GUILD_ID)
+)
+async def leave(interaction: discord.Interaction):
+    if interaction.guild.voice_client is None:
+        await interaction.response.send_message(f"I'm not on a voice channel.")
         return
 
-    channel = ctx.author.voice.channel
+    channel = interaction.user.voice.channel
 
-    await ctx.voice_client.disconnect()
-    await ctx.reply(f"I successfully left the **{channel}** channel!")
+    await interaction.guild.voice_client.disconnect()
+    await interaction.response.send_message(f"I successfully left the **{channel}** channel!")
 
 # contador de tempo
 @bot.event
@@ -95,44 +142,48 @@ async def on_voice_state_update(member, before, after):
         times.pop(member.id, None)
 
 # pede o tempo de permanência em um canal
-@bot.command()
-async def time(ctx):
-    if ctx.author.id not in times:
-        await ctx.reply("You've not entered any voice chat recently.")
+# TODO: implementar sistema de tempo em cada canal
+@tree.command(
+        name="time",
+        description="the bot shows you the time spent in a determined voice channel.",
+        guild=discord.Object(id=GUILD_ID)
+)
+async def time(interaction: discord.Interaction):
+    if interaction.author.id not in times:
+        await interaction.response.send_message("You've not entered any voice chat recently.")
         return
     
     now = datetime.datetime.now()
-    entryTime = times[ctx.author.id]
+    entryTime = times[interaction.author.id]
 
     time = now - entryTime
     total_seconds = int(time.total_seconds())
     hours = total_seconds // 3600
     minutes = (total_seconds % 3600) // 60
-    seconds = total_seconds % 60    
+    seconds = total_seconds % 60  
 
-    # TODO: traduzir isso dps, que preguiça
     if hours > 0:
         if minutes > 0:
             if seconds > 0:
-                await ctx.reply(f"You have been in the channel for {hours}h, {minutes}min and {seconds}s.")
+                await interaction.response.send_message(f"You have been in the channel for {hours}h, {minutes}min and {seconds}s.")
             else:
-                await ctx.reply(f"You have been in the channel for {hours}h and {minutes}min.")
+                await interaction.response.send_message(f"You have been in the channel for {hours}h and {minutes}min.")
         else:
             if seconds > 0:
-                await ctx.reply(f"You have been in the channel for {hours}h and {seconds}s.")
+                await interaction.response.send_message(f"You have been in the channel for {hours}h and {seconds}s.")
             else:
-                await ctx.reply(f"You have been in the channel for {hours}h.")
+                await interaction.response.send_message(f"You have been in the channel for {hours}h.")
     else:
         if minutes > 0:
             if seconds > 0:
-                await ctx.reply(f"You have been in the channel for {minutes}min and {seconds}s.")
+                await interaction.response.send_message(f"You have been in the channel for {minutes}min and {seconds}s.")
             else:
-                await ctx.reply(f"You have been in the channel for {minutes}min.")
+                await interaction.response.send_message(f"You have been in the channel for {minutes}min.")
         else:
             if seconds > 0:
-                await ctx.reply(f"You have been in the channel for {seconds}s.")
+                await interaction.response.send_message(f"You have been in the channel for {seconds}s.")
             else:
-                await ctx.reply("You have just entered the channel.")
+                await interaction.response.send_message("You have just entered the channel.")
 
 
 bot.run(TOKEN)
